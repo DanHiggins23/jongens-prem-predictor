874542199ef1986fc3f0dec0986ac72954dc9bf6
DanHiggins23 <danhiggins23@outlook.com>
2023-08-08 22:08:05 +0100

    Reorganising app, adding score and user const for all details

---
+++ src/components/App.js
@@ -0,0 +1,257 @@
+import axios from "axios";
+import "./App.scss";
+import { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Avatar from "@mui/material/Avatar";
+import defaultProfile from "../../src/images/default-pp.jpg";
+import users from "../utils/users";
+
+function App() {
+  const [standings, setStandings] = useState([]);
+  const [sortedResults, setSortedResults] = useState([]);
+
+  const getStandings = async () => {
+    const response = await axios.get(
+      "https://premier-league-standings1.p.rapidapi.com/?season=2023",
+      {
+        headers: {
+          "X-RapidAPI-Key":
+            "5d4abb2db7msh48ef7358e10d30fp15bcb3jsn00bc1450501f",
+          "X-RapidAPI-Host": "premier-league-standings1.p.rapidapi.com",
+        },
+      }
+    );
+    return response.data;
+  };
+
+  const requestStandings = () => {
+    getStandings()
+      .then((response) => {
+        setStandings(response);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const calculateScores = () => {
+    const scores = [];
+
+    users.forEach((user, userIndex) => {
+      let score = 0;
+      standings.forEach((standing, standingIndex) => {
+        if (standing.team.name === user.predictions[standingIndex]) {
+          score = score + 5;
+        }
+        score = score - 1;
+      });
+      scores.push({ name: user.name, score: score, profile: user.profile });
+    });
+
+    return scores;
+  };
+
+  useEffect(() => {
+    if (standings.length > 0) {
+      setSortedResults(calculateScores().sort((a, b) => b.score - a.score));
+    }
+  }, [standings]);
+
+  return (
+    <div className="App">
+      <>
+        <h1>Jongens Op Tournee: Prem Predictor</h1>
+
+        <div style={{ display: "flex", justifyContent: "center", gap: "40px" }}>
+          <div>
+            <Avatar
+              alt={sortedResults[0]?.name}
+              src={
+                sortedResults.length > 0
+                  ? sortedResults[0]?.profile
+                  : defaultProfile
+              }
+              sx={{ width: 120, height: 120 }}
+            />
+            <p>🍺🍺🍺🍺🍺</p>
+          </div>
+          <div>
+            <Avatar
+              alt={sortedResults[sortedResults.length]?.name}
+              src={
+                sortedResults.length > 0
+                  ? sortedResults[sortedResults.length - 1]?.profile
+                  : defaultProfile
+              }
+              sx={{ width: 120, height: 120 }}
+            />
+            <p>💷💷💷💷💷</p>
+          </div>
+        </div>
+      </>
+
+      <Button
+        variant="contained"
+        onClick={() => requestStandings()}
+        sx={{ backgroundColor: "#F2055C", marginBottom: "20px" }}
+      >
+        {standings.length > 0 ? "Update score" : "Calculate score"}
+      </Button>
+
+      {sortedResults.length > 0 && (
+        <>
+          <h2>Scores on the doors</h2>
+
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell
+                    align="left"
+                    sx={{ color: "#340040", fontWeight: "bold" }}
+                  >
+                    Pos
+                  </TableCell>
+                  <TableCell
+                    align="left"
+                    sx={{ color: "#340040", fontWeight: "bold" }}
+                  >
+                    Jongen
+                  </TableCell>
+                  <TableCell
+                    align="center"
+                    sx={{ color: "#340040", fontWeight: "bold" }}
+                  >
+                    Points
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {sortedResults.map((result, index) => (
+                  <TableRow>
+                    <TableCell
+                      align="left"
+                      sx={{ color: "#340040", fontWeight: "bold" }}
+                    >
+                      {index + 1}
+                    </TableCell>
+                    <TableCell align="left" sx={{ color: "#340040" }}>
+                      <div
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                          gap: "15px",
+                        }}
+                      >
+                        <img
+                          src={result.profile}
+                          width="30px"
+                          height="30px"
+                          alt="Jongen"
+                          style={{ borderRadius: "50px" }}
+                        />
+                        {result.name}
+                      </div>
+                    </TableCell>
+                    <TableCell align="center" sx={{ color: "#340040" }}>
+                      {result.score}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </>
+      )}
+
+      {standings.length > 0 && (
+        <>
+          <h2>League table</h2>
+
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell
+                    align="center"
+                    sx={{ color: "#340040", fontWeight: "bold" }}
+                  >
+                    Pos
+                  </TableCell>
+                  <TableCell
+                    align="left"
+                    sx={{ color: "#340040", fontWeight: "bold" }}
+                  >
+                    Club
+                  </TableCell>
+                  <TableCell
+                    align="center"
+                    sx={{ color: "#340040", fontWeight: "bold" }}
+                  >
+                    Pld
+                  </TableCell>
+                  <TableCell
+                    align="center"
+                    sx={{ color: "#340040", fontWeight: "bold" }}
+                  >
+                    GD
+                  </TableCell>
+                  <TableCell
+                    align="center"
+                    sx={{ color: "#340040", fontWeight: "bold" }}
+                  >
+                    Pts
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {standings.map((team) => (
+                  <TableRow>
+                    <TableCell
+                      align="center"
+                      sx={{ color: "#340040", fontWeight: "bold" }}
+                    >
+                      {team.stats.rank}
+                    </TableCell>
+                    <TableCell align="left" sx={{ color: "#340040" }}>
+                      <div
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                          gap: "15px",
+                        }}
+                      >
+                        <img
+                          src={team.team.logo}
+                          width="30px"
+                          alt="Prem team logo"
+                        />
+                        {team.team.name}
+                      </div>
+                    </TableCell>
+                    <TableCell align="center" sx={{ color: "#340040" }}>
+                      {team.stats.gamesPlayed}
+                    </TableCell>
+                    <TableCell align="center" sx={{ color: "#340040" }}>
+                      {team.stats.goalDifference}
+                    </TableCell>
+                    <TableCell align="center" sx={{ color: "#340040" }}>
+                      {team.stats.points}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/users.js
@@ -0,0 +1,79 @@
+import meProfile from "../../src/images/me-pp.jpg";
+import jonnyProfile from "../../src/images/jonny-pp.jpg";
+import shandogProfile from "../../src/images/shandog-pp.jpg";
+import zachProfile from "../../src/images/zach-pp.jpg";
+import dcProfile from "../../src/images/dc-pp.jpg";
+
+const users = [
+  {
+    predictions: [
+      "Manchester City",
+      "Arsenal",
+      "Liverpool",
+      "Newcastle United",
+      "Cheslea",
+      "Manchester United",
+      "Aston Villa",
+      "Tottenham Hotspur",
+      "Brighton & Hove Albion",
+      "West Ham United",
+      "Crystal Palace",
+      "Brentford",
+      "Fulham",
+      "Nottingham Forest",
+      "AFC Bournemouth",
+      "Burnley",
+      "Luton Town",
+      "Everton",
+      "Wolverhampton Wanderers",
+      "Sheffield United",
+    ],
+    goals: 1054,
+    name: "Higgo",
+    profile: meProfile,
+  },
+  {
+    predictions: [],
+    name: "Jonboi",
+    profile: jonnyProfile,
+  },
+  {
+    predictions: [],
+    name: "Shandog",
+    profile: shandogProfile,
+  },
+  {
+    predictions: [
+      "Manchester City",
+      "Liverpool",
+      "Arsenal",
+      "Manchester United",
+      "Newcastle United",
+      "Cheslea",
+      "Tottenham Hotspur",
+      "Brighton & Hove Albion",
+      "Aston Villa",
+      "West Ham United",
+      "Brentford",
+      "Crystal Palace",
+      "Wolverhampton Wanderers",
+      "Fulham",
+      "Everton",
+      "Nottingham Forest",
+      "Burnley",
+      "AFC Bournemouth",
+      "Sheffield United",
+      "Luton Town",
+    ],
+    goals: 969,
+    name: "Zachamo",
+    profile: zachProfile,
+  },
+  {
+    predictions: [],
+    name: "DC",
+    profile: dcProfile,
+  },
+];
+
+export default users;
