772df9f4a9830f32243c0cb7a6a157f7ac5eba16
Dan Higgins <danhiggins@Dans-MacBook-Air.local>
2025-07-16 12:53:14 +0100

    Adding ability to save prediction and updating build directory

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
+import App from "./components/App.jsx";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
